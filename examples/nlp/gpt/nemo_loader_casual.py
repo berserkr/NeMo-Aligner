7ed96099345a1ccb1b66dfcf0b6875095c1ad14b
LUIS ANGEL BATHEN <bathen@us.ibm.com>
2024-08-16 10:06:58 -0700

    Added codebase to allow for better instruct inference

--- examples/nlp/gpt/nemo_loader_casual.py
+++ examples/nlp/gpt/nemo_loader_casual.py
@@ -2,18 +2,16 @@ import torch
 
 from pytorch_lightning.trainer.trainer import Trainer
 
-from nemo.collections.nlp.parts.nlp_overrides import NLPDDPStrategy
 from nemo.core.config import hydra_runner
-
 from nemo.collections.nlp.models.language_modeling.megatron_gpt_model import MegatronGPTModel
 from nemo.collections.nlp.parts.nlp_overrides import NLPDDPStrategy
-from nemo.collections.nlp.modules.common.transformer.text_generation import LengthParam
+from nemo.collections.nlp.modules.common.transformer.text_generation import LengthParam, SamplingParam
 from nemo.collections.nlp.modules.common.lm_utils import pad_batch
 
 from nemo_aligner.utils.train_script_utils import init_distributed
 from nemo_aligner.utils.utils import load_and_override_model_config, set_autocast_gpu_dtype
 from nemo_aligner.utils import parallel_state
-from nemo_aligner.utils.distributed import broadcast_2d_tensor, rebalance_nd_tensor
+from nemo_aligner.utils.distributed import broadcast_2d_tensor
 from nemo_aligner.utils.server_utils import (
     calculate_inference_batch_padding_multiple,
     pad_batch_and_strip_sequence
@@ -33,7 +31,7 @@ def print_rank_0(message):
         print(message, flush=True)
 
 
-def run_distributed_generate(inputs=None, length_params=None, generate_fn=None):
+def run_distributed_generate(inputs=None, length_params=None, sampling_params=None, generate_fn=None):
     tokens, lengths = None, None
     dp_rank = parallel_state.get_data_parallel_rank()
     dp_size = parallel_state.get_data_parallel_world_size()
@@ -46,48 +44,21 @@ def run_distributed_generate(inputs=None, length_params=None, generate_fn=None):
     tokens = broadcast_2d_tensor(tokens, 0, dtype=torch.long, group=None).chunk(dp_size)[dp_rank]
     lengths = broadcast_2d_tensor(lengths, 0, dtype=torch.long, group=None).chunk(dp_size)[dp_rank].squeeze(-1)
 
-    outputs = generate_fn(inputs=(tokens, lengths), length_params=length_params)
+    outputs = generate_fn(inputs=(tokens, lengths), length_params=length_params, sampling_params=sampling_params)
 
-    return outputs #rebalance_nd_tensor(outputs, group=parallel_state.get_data_parallel_group()).cpu().numpy()
+    return outputs 
 
 
-@hydra_runner(config_path="conf", config_name="inference_rm")
-def main(cfg) -> None:
-
-    # Load the model
-    cfg.model = load_and_override_model_config(cfg.rm_model_file, cfg.model)
-    trainer = Trainer(strategy=NLPDDPStrategy(), **cfg.trainer)
-
-    set_autocast_gpu_dtype(cfg.trainer.precision)
-    if trainer.precision == "16":
-        cfg.model.megatron_amp_O2 = False
-    elif trainer.precision in ["bf16", "bf16-mixed"] and cfg.get("megatron_amp_O2", False):
-        cfg.model.megatron_amp_O2 = True
-
-    ptl_model = MegatronGPTModel.restore_from(
-        cfg.rm_model_file, trainer=trainer, override_config_path=cfg.model
-    )
-
-    ptl_model.freeze()
-
-    # Have to turn off activations_checkpoint_method for inference
-    try:
-        ptl_model.model.language_model.encoder.activations_checkpoint_method = None
-    except AttributeError:
-        pass
-
-    init_distributed(trainer, ptl_model, cfg.model.get("transformer_engine", False))
-    ptl_model = ptl_model.cuda()
-    model_forward_micro_batch_size = cfg.model.get("forward_micro_batch_size", cfg.model.micro_batch_size)
-    strip_sequence_length_to_multiple = cfg.inference.get("strip_sequence_length_to_multiple", None)
-
-    tokenizer = MistralTokenizer.v3(is_tekken=True)
-
-    prompt = "How expensive would it be to ask a window cleaner to clean all windows in Paris. Make a reasonable guess in US Dollar."
-    completion_request = ChatCompletionRequest(messages=[UserMessage(content=prompt)])
-
-    sample_batch_size = 8
-    sentences = [completion_request] * sample_batch_size
+def generate(
+        generate_fn, 
+        tokenizer, 
+        model_forward_micro_batch_size, 
+        strip_sequence_length_to_multiple, 
+        length_params, 
+        max_len,
+        sampling_params,
+        sentences
+    ):
 
     def tokenize(sentence):
         tokens = tokenizer.encode_chat_completion(sentence).tokens
@@ -95,9 +66,6 @@ def main(cfg) -> None:
 
     context_tokens = list(map(tokenize, sentences))
     max_sequence_length = max(len(x) for x in context_tokens)
-    max_len = 4096 # seq len
-
-    length_params: LengthParam = {"min_length": 0, "max_length": max_len}
 
     context_tokens, context_lengths = pad_batch(
         context_tokens, 
@@ -123,7 +91,85 @@ def main(cfg) -> None:
         strip_sequence_length_to_multiple=strip_sequence_length_to_multiple,
     )
 
-    outputs = run_distributed_generate(inputs=inputs, generate_fn=ptl_model.generate, length_params=length_params)    
+    outputs = run_distributed_generate(
+        inputs=inputs, 
+        generate_fn=generate_fn, 
+        length_params=length_params,
+        sampling_params=sampling_params)
+
+    return outputs
+
+
+@hydra_runner(config_path="conf", config_name="inference_mistral_instruct")
+def main(cfg) -> None:
+
+    # Load the model
+    cfg.model = load_and_override_model_config(cfg.model_file, cfg.model)
+    trainer = Trainer(strategy=NLPDDPStrategy(), **cfg.trainer)
+
+    set_autocast_gpu_dtype(cfg.trainer.precision)
+    if trainer.precision == "16":
+        cfg.model.megatron_amp_O2 = False
+    elif trainer.precision in ["bf16", "bf16-mixed"] and cfg.get("megatron_amp_O2", False):
+        cfg.model.megatron_amp_O2 = True
+
+    ptl_model = MegatronGPTModel.restore_from(
+        cfg.model_file, trainer=trainer, override_config_path=cfg.model
+    )
+
+    ptl_model.freeze()
+
+    # Have to turn off activations_checkpoint_method for inference
+    try:
+        ptl_model.model.language_model.encoder.activations_checkpoint_method = None
+    except AttributeError:
+        pass
+    
+    # initialize the model for inferencing
+    init_distributed(trainer, ptl_model, cfg.model.get("transformer_engine", False))
+    ptl_model = ptl_model.cuda()
+    model_forward_micro_batch_size = cfg.model.get("forward_micro_batch_size", cfg.model.micro_batch_size)
+    strip_sequence_length_to_multiple = cfg.inference.get("strip_sequence_length_to_multiple", None)
+
+    # use v3 tokenizer for instruct model
+    tokenizer = MistralTokenizer.v3(is_tekken=True)
+
+    prompt = "How expensive would it be to ask a window cleaner to clean all windows in Paris. Make a reasonable guess in US Dollar."
+    completion_request = ChatCompletionRequest(messages=[UserMessage(content=prompt)])
+
+    sample_batch_size = 8
+    sentences = [completion_request] * sample_batch_size
+
+    max_len = 4096 # seq len
+
+    length_params: LengthParam = {
+        "max_length": cfg.inference.tokens_to_generate,
+        "min_length": cfg.inference.min_tokens_to_generate,
+    }
+
+    sampling_params: SamplingParam = {
+        "use_greedy": cfg.inference.greedy,
+        "temperature": cfg.inference.temperature,
+        "top_k": cfg.inference.top_k,
+        "top_p": cfg.inference.top_p,
+        "repetition_penalty": cfg.inference.repetition_penalty,
+        "add_BOS": cfg.inference.add_BOS,
+        "all_probs": cfg.inference.all_probs,
+        "compute_logprob": cfg.inference.compute_logprob,
+        "end_strings": cfg.inference.end_strings,
+    }
+
+    outputs = generate(
+        generate_fn=ptl_model.generate, 
+        tokenizer=tokenizer, 
+        model_forward_micro_batch_size=model_forward_micro_batch_size,
+        strip_sequence_length_to_multiple=strip_sequence_length_to_multiple,
+        length_params=length_params,
+        max_len=max_len,
+        sampling_params=sampling_params,
+        sentences=sentences
+        )    
+    
     print_rank_0(outputs['sentences'])
 
 
